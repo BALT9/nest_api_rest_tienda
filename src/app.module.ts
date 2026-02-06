f1e1c5d51b77e28c6f22f600cf0461585b620353
willy <willybalt.2017@gmail.com>
2026-02-05 20:04:39 -0400

    configuracion inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,24 @@ import { AppService } from './app.service';
 
 // importa modulo de archivo .env
 import { ConfigModule } from '@nestjs/config';
+// importa Type ORM
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: +`${process.env.DATABASE_PORT}` || 3306,
+      username: process.env.DATABASE_USER || 'root',
+      password: process.env.DATABASE_PASSWORD || '',
+      database: process.env.DATABASE_NAME || 'bd_back_nest_api',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
