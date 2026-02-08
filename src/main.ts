e2ce8eb45e241f717db660c80b36d9e6d64920ac
willy <willybalt.2017@gmail.com>
2026-02-08 15:25:31 -0400

    jwt y swager

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('API REST BACKEND NEST')
+    .setDescription('API backend')
+    .setVersion('1.0')
+    .addTag('code')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../admin/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
     imports: [
         UsersModule,
         JwtModule.register({
             global: true,
-            secret: "MI_CODIGO_SECRETO",
+            secret: jwtConstants.secret,
             signOptions: { expiresIn: '60s' },
         }),
     ],
