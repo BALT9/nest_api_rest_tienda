036c2ac8a721f905e210f57540d03961770039df
willy <willybalt.2017@gmail.com>
2026-02-16 17:28:00 -0400

    relacion sucursal almacen

--- src/modules/admin/inventario/almacen/entities/almacen.entity.ts
+++ src/modules/admin/inventario/almacen/entities/almacen.entity.ts
@@ -1,1 +1,21 @@
-export class Almacen {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Sucursal } from "../../sucursal/entities/sucursal.entity";
+
+@Entity('almacenes')
+export class Almacen {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 100 })
+    nombre: string;
+
+    @Column({ length: 100, nullable: true })
+    codigo: string;
+
+    @Column({ type: 'text', nullable: true })
+    descripcion: string;
+
+    @ManyToOne(() => Sucursal, sucursal => sucursal.almacenes, { eager: true })
+    sucursal: Sucursal;
+}
--- src/modules/admin/inventario/sucursal/entities/sucursal.entity.ts
+++ src/modules/admin/inventario/sucursal/entities/sucursal.entity.ts
@@ -1,1 +1,20 @@
-export class Sucursal {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Almacen } from "../../almacen/entities/almacen.entity";
+
+@Entity('sucursales')
+export class Sucursal {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 100 })
+    nombre: string;
+
+    @Column({ length: 225 })
+    direccion: string;
+
+    @Column({ length: 100 })
+    ciudad: string;
+
+    @OneToMany(()=> Almacen, almacen => almacen.sucursal)
+    almacenes: Almacen[];
+}
