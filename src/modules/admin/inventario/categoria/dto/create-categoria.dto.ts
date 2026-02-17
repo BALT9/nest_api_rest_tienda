108002a5ce8ef442ea40e14f48f05f55f366c003
willy <willybalt.2017@gmail.com>
2026-02-17 15:12:07 -0400

    crud categoria

--- src/modules/admin/inventario/categoria/dto/create-categoria.dto.ts
+++ src/modules/admin/inventario/categoria/dto/create-categoria.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCategoriaDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateCategoriaDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    nombre: string;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    descripcion?: string;
+
+}
