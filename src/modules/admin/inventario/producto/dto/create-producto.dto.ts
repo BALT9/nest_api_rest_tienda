2bc2e52b4cfb5944969feee4fef20f29e9e550b4
willy <willybalt.2017@gmail.com>
2026-02-22 10:08:09 -0400

    dto producto

--- src/modules/admin/inventario/producto/dto/create-producto.dto.ts
+++ src/modules/admin/inventario/producto/dto/create-producto.dto.ts
@@ -1,1 +1,45 @@
-export class CreateProductoDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsDecimal, IsInt, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateProductoDto {
+
+    @ApiProperty()
+    @IsString()
+    @MaxLength(200)
+    @IsNotEmpty()
+    nombre: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    descripcion: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    unidad_medida?: string;
+
+    @ApiProperty()
+    @IsString()
+    @MaxLength(200)
+    @IsNotEmpty()
+    marca?: string;
+
+    @ApiProperty()
+    @IsDecimal()
+    precio_venta_actual: number;
+
+    @ApiProperty()
+    @IsString()
+    @MaxLength(250)
+    @IsOptional()
+    imagen?: string;
+
+    @ApiProperty()
+    @IsBoolean()
+    estado: boolean;
+
+    @ApiProperty()
+    @IsInt()
+    categoria: number;
+}
--- src/modules/admin/inventario/producto/producto.service.ts
+++ src/modules/admin/inventario/producto/producto.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { UpdateProductoDto } from './dto/update-producto.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Producto } from './entities/producto.entity';
+import { Repository } from 'typeorm';
+import { Categoria } from '../categoria/entities/categoria.entity';
 
 @Injectable()
 export class ProductoService {
+
+  constructor(
+    @InjectRepository(Producto)
+    private productoRepository: Repository<Producto>,
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ){}
+
   create(createProductoDto: CreateProductoDto) {
     return 'This action adds a new producto';
   }
