22e7a54a89e06e38eb8677b3d272fa7876bc18be
willy <willybalt.2017@gmail.com>
2026-02-28 16:14:36 -0400

    crud almacen

--- src/modules/admin/inventario/almacen/dto/create-almacen.dto.ts
+++ src/modules/admin/inventario/almacen/dto/create-almacen.dto.ts
@@ -1,1 +1,21 @@
-export class CreateAlmacenDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateAlmacenDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    nombre: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    codigo: string;
+
+    @IsOptional()
+    @IsString()
+    descripcion?: string;
+
+    @IsInt()
+    sucursal: number;
+}
