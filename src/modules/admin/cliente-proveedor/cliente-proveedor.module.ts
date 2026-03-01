3e265148019e7f842b2a7d52d64f4e948ad4ca10
willy <willybalt.2017@gmail.com>
2026-03-01 14:12:55 -0400

    ajuste en service cliente-proveedor

--- src/modules/admin/cliente-proveedor/cliente-proveedor.module.ts
+++ src/modules/admin/cliente-proveedor/cliente-proveedor.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteProveedorService } from './cliente-proveedor.service';
 import { ClienteProveedorController } from './cliente-proveedor.controller';
+import { ClienteProveedor } from './entities/cliente-proveedor.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ClienteProveedor]), // <-- Esto habilita el Repository
+  ],
   controllers: [ClienteProveedorController],
   providers: [ClienteProveedorService],
 })
