d65345ff7874ef68ad7fbfaef83607a05d0b3a1c
willy <willybalt.2017@gmail.com>
2026-05-01 17:35:30 -0400

    enpoind profile

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,16 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
 
-    constructor(private authService: AuthService){
+    constructor(private authService: AuthService) {
 
     }
 
     @Post("/login")
-    funLogin(@Body() datos: LoginAuthDto){
+    funLogin(@Body() datos: LoginAuthDto) {
         return this.authService.signIn(datos.email, datos.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('/profile')
+    getProfile(@Req() req: any) {
+        return req.user;
     }
 }
